c4839818628adbbf219f7c5093a6169698d33c54
Samuel Richards <richards.m.samuel@gmail.com>
2023-06-28 18:22:50 +1200

    Added simple authentication for flat pages rerouting user if they are not part of flat/{id}

--- src/pages/flat/[flat_id].tsx
+++ src/pages/flat/[flat_id].tsx
@@ -1,19 +1,47 @@
-import type { NextPage} from "next";
-import Head from "next/head";
-import { api } from "~/utils/api"
-const FlatPage: NextPage<{ id: number }> = ({ id }) => {
-    const { data } = api.flat.getUserFlatByFlatIdAndContext.useQuery({id});
-    if (!data) return <div>404</div>;
+import { useRouter } from 'next/router';
+import { NextPage } from 'next';
+import { api } from '~/utils/api';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { LoadingPage } from '~/components/loading';
 
-    return (
-      <>
-        <Head>
-          <title>{`${data.flatId} - @${data.userId}`}</title>
-        </Head>
-        <body>
-          <h1>{`Flatview for:${data.flatId} User:${data.userId}`}</h1>
-        </body>
-      </>
-    );
-  };
-  export default FlatPage;
+const FlatPage: NextPage = () => {
+  const router = useRouter();
+  const flat_id = Array.isArray(router.query.flat_id)
+    ? Number(router.query.flat_id[0])
+    : Number(router.query.flat_id);
+
+  const { data: userFlatData, isLoading } = api.flat.getUserFlatByFlatIdAndContext.useQuery({
+    id: flat_id
+  });
+
+  useEffect(() => {
+    if (!isLoading && userFlatData && Number(flat_id) !== userFlatData.flatId) {
+      // If the user's flatId does not match the flat_id from the URL, redirect to home
+      router.push("/");
+    }
+  }, [isLoading, userFlatData, flat_id, router]);
+
+  if (isLoading) {
+    return <div>
+      <LoadingPage />
+    </div>;
+  }
+
+  if (!userFlatData) {
+    return <div>404</div>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{`${userFlatData.flatId} - @${userFlatData.userId}`}</title>
+      </Head>
+      <div>
+        <h1>{`Flatview for:${userFlatData.flatId} User:${userFlatData.userId}`}</h1>
+      </div>
+    </>
+  );
+};
+
+export default FlatPage;
