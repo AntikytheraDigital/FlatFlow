eaf8d0deb8b5e44667752c67f196c751996caedb
Samuel Richards <richards.m.samuel@gmail.com>
2023-06-29 14:47:27 +1200

    Updated flat view test page to correctly use composite useridflatid schema for find unique

--- src/pages/flat/[flat_id].tsx
+++ src/pages/flat/[flat_id].tsx
@@ -1,12 +1,13 @@
-import { useRouter } from "next/router";
-import { type NextPage } from "next";
-import { api } from "~/utils/api";
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import { LoadingPage } from "~/components/loading";
+import { useRouter } from 'next/router';
+import { NextPage } from 'next';
+import { api } from '~/utils/api';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { LoadingPage } from '~/components/loading';
 
 const FlatPage: NextPage = () => {
   const router = useRouter();
+  //flat_id is a string. needs to be converted to number for prisma query
   const flat_id = Array.isArray(router.query.flat_id)
     ? Number(router.query.flat_id[0])
     : Number(router.query.flat_id);
@@ -16,35 +17,21 @@ const FlatPage: NextPage = () => {
   });
 
   useEffect(() => {
-    if (router.isReady) {
-      const flatIdFromRouter = Number(router.query.flat_id);
-      if (isNaN(flatIdFromRouter)) return;
-
-      setFlatId(flatIdFromRouter);
-
-      if (
-        !isLoading &&
-        userFlatData &&
-        flatIdFromRouter !== userFlatData.flatId
-      ) {
-        // If the user's flatId does not match the flat_id from the URL, redirect to home. This should have error handling for promise.
-        void router.push("/");
-      }
+    if (!isLoading && (!userFlatData || flat_id !== userFlatData.flatId)) {
+      // If the user doesn't have a flatId, or if the user's flatId does not match the flat_id from the URL, redirect to home
+      router.push("/");
     }
-  }, [isLoading, userFlatData, router]);
+  }, [isLoading, userFlatData, flat_id, router]);
+  
 
-  // Show loading until we have all necessary data and checks
-  if (
-    isLoading ||
-    !userFlatData ||
-    flatId === null ||
-    flatId !== userFlatData.flatId
-  ) {
-    return (
-      <div>
-        <LoadingPage />
-      </div>
-    );
+  if (isLoading || isNaN(flat_id)) {
+    return <div>
+      <LoadingPage />
+    </div>;
+  }
+
+  if (!userFlatData) {
+    return <div>404</div>;
   }
 
   return (
@@ -53,11 +40,11 @@ const FlatPage: NextPage = () => {
         <title>{`${userFlatData.flatId} - @${userFlatData.userId}`}</title>
       </Head>
       <div>
-        <h1>{`Flatview for: ${userFlatData.flatId} User: ${userFlatData.userId}`}</h1>
+        <h1>{`Flatview for user belonging to: ${userFlatData.flatId} User: ${userFlatData.userId}`}</h1>
       </div>
       <div>
-        <h2>{`Flat page: ${flatId}`}</h2>
-      </div>
+        <h2>{`Flat page view for flatid: ${flat_id}`}</h2>
+        </div>
     </>
   );
 };
