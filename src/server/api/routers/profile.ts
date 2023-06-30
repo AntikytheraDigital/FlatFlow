c6ef7a2a41276c9ddbf189f0fedc67eb56ba4b47
walterpete1 <walterpete1@myvuw.ac.nz>
2023-06-30 15:30:48 +1200

    Stub profile router and create flat adds user tuple

---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,9 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const profileRouter = createTRPCRouter({});
