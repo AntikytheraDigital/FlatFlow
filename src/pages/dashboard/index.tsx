ff546d8b1004d78ebfa038a3b5dfa0e3be7307bc
Samuel Richards <richards.m.samuel@gmail.com>
2023-07-04 15:03:57 +1200

    Changed user and userflat schema to include flat names

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -13,6 +13,65 @@ import { api } from "@/utils/api";
 
 type InputFieldProps = ButtonProps;
 
+const CreateFlat = () => {
+  const { user } = useUser();
+  const [flatName, setFlatName] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  
+  const router = useRouter(); 
+
+  const { mutate, isLoading: isCreating } =
+    api.flat.createFlatWithName.useMutation({
+      onSuccess: (flat) => {
+        setSuccessMessage(`Successfully created flat: ${flat.id}`);
+        setFlatName("");
+        router.push(`/flat/${flat.id}`);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+
+  const handleCreateFlat = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!user || !flatName) return;
+
+    try {
+      mutate({ name: flatName });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="grid w-full max-w-sm items-center space-x-2">
+      <Label htmlFor="flatName">Create flat</Label>
+      <Input 
+        type="text"
+        id="flatName"
+        value={flatName}
+        onChange={(event) => setFlatName(event.target.value)}
+        disabled={isCreating}
+        placeholder="Flat Name" 
+      />
+      <Button 
+        onClick={handleCreateFlat}
+        disabled={isCreating}
+        variant="default"
+      >
+        {isCreating ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.add className="mr-2 h-4 w-4" />
+        )}
+        Create flat
+      </Button>
+      {successMessage && <p>{successMessage}</p>}
+    </div>
+  );
+};
+
 const JoinFlat = () => {
   const { user } = useUser();
   const [flatId, setFlatId] = useState("");
@@ -81,6 +140,9 @@ export default function Dashboard() {
       </div>
       <div>
         <JoinFlat />
+      </div>
+      <div>
+        <CreateFlat />
       </div>
     </>
   );
