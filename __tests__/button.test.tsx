8ea367577702446b85ad3ed1b40d44e1e36e56ed
walterpete1 <walterpete1@myvuw.ac.nz>
2023-07-05 16:21:32 +1200

    .

--- __tests__/button.test.tsx
+++ __tests__/button.test.tsx
@@ -1,23 +1,54 @@
-import { cleanup, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
-afterEach(() => {
-  cleanup();
-});
-
-//This test should always pass
-test("two plus two is four", () => {
-  expect(2 + 2).toBe(4);
-});
-
-describe("Button Component", () => {
-  const onclick = jest.fn();
-  render(<Button onClick={onclick}>Sample Text</Button>);
-  const button = screen.getByTestId("button");
-
-  test("Button Text", () => {
-    expect(button).toHaveTextContent("Sample Text");
+describe("Button component", () => {
+  it("renders correctly", () => {
+    render(<Button>Click me</Button>);
+    const buttonElement = screen.getByTestId("button");
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveTextContent("Click me");
   });
+
+  it("applies the correct variant", () => {
+    render(<Button variant="destructive">Delete</Button>);
+    const buttonElement = screen.getByTestId("button");
+    expect(buttonElement).toHaveClass("bg-destructive");
+    expect(buttonElement).toHaveClass("text-destructive-foreground");
+  });
+
+  it("triggers onClick handler", () => {
+    const handleClick = jest.fn();
+    render(<Button onClick={handleClick}>Click me</Button>);
+    const buttonElement = screen.getByTestId("button");
+
+    buttonElement.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("renders with additional className", () => {
+    render(<Button className="custom-class">Custom Button</Button>);
+    const buttonElement = screen.getByTestId("button");
+    expect(buttonElement).toHaveClass("custom-class");
+  });
+
+  it("renders with custom size", () => {
+    render(<Button size="sm">Small Button</Button>);
+    const buttonElement = screen.getByTestId("button");
+    expect(buttonElement).toHaveClass("h-9");
+    expect(buttonElement).toHaveClass("px-3");
+    expect(buttonElement).toHaveClass("rounded-md");
+  });
+
+  it("triggers onFocus handler", () => {
+    const handleFocus = jest.fn();
+    render(<Button onFocus={handleFocus}>Button</Button>);
+    const buttonElement = screen.getByTestId("button");
+    buttonElement.focus();
+    expect(handleFocus).toHaveBeenCalledTimes(1);
+  });
+
+  // Add more test cases as needed
 });
