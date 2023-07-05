df09ea1d0cf2208831a610cf62701bb97c3f6e45
walterpete1 <walterpete1@myvuw.ac.nz>
2023-07-05 13:51:53 +1200

    .

--- __tests__/profile.test.ts
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import { ssgHelper } from "@/server/api/helpers/ssgHelper";
-
-const ssg = await ssgHelper();
-
-test("two plus two is four", () => {
-  expect(2 + 2).toBe(4);
-});
